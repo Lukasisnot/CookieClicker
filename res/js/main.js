b1b92401be4b4dbe5105d17946cc00d87741429b
LukasIs <lukas.pav0123@gmail.com>
2023-09-27 10:29:48 +0200

    new basic upgrade system

--- res/js/main.js
+++ res/js/main.js
@@ -1,11 +1,26 @@
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
+const upgradeButton = document.getElementById("upgradeButton");
 
 let numberOfCookies = 0;
+let cookieAdder = 1;
+let upgradeCost = 50;
 
 cookie.onclick = () => {
   console.log("click");
-  numberOfCookies++;
+  numberOfCookies += cookieAdder;
   console.log(numberOfCookies);
   counter.innerHTML = "Cookies: " + numberOfCookies;
+};
+
+upgradeButton.onclick = () => {
+  if (numberOfCookies >= 50) {
+    numberOfCookies -= 50;
+    cookieAdder *= 2;
+    upgradeButton.ariaDisabled = true;
+    counter.innerHTML = "Cookies: " + numberOfCookies;
+
+    upgradeCost *= 3;
+    upgradeButton.innerHTML = "Upgrade: " + upgradeCost;
+}
 };
